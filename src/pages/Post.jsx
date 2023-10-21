d85864c72f15056863c464ce5dc58a30820bc2e8
simo-sw <simonecapelli13@gmail.com>
2023-10-21 17:58:19 +0200

    Fetch all posts and single one

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchPost } from '../api/posts';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const Post = () => {
-  return <div>Post</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const {
+    isLoading,
+    isError,
+    data: post,
+    error,
+  } = useQuery({
+    queryKey: ['posts', id],
+    queryFn: () => fetchPost(id),
+  });
+  if (isLoading) return 'Loading...';
+  if (isError) return `Error: ${error.message}`;
+
+  return (
+    <div>
+      <button onClick={() => navigate('/')}>Back to list posts</button>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default Post;
--- src/pages/PostLists.jsx
+++ src/pages/PostLists.jsx
@@ -1,9 +1,49 @@
+import { useQuery } from '@tanstack/react-query';
 import AddPost from '../components/AddPost';
+import { fetchPosts } from '../api/posts';
+import { useNavigate } from 'react-router-dom';
 
 const PostLists = () => {
+  const navigate = useNavigate();
+
+  const {
+    isLoading,
+    isError,
+    data: posts,
+    error,
+  } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+  if (isLoading) return 'Loading...';
+  if (isError) return `Error: ${error.message}`;
+
   return (
     <div>
-      <AddPost /> lists
+      <AddPost />
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          style={{
+            border: '1px solid black',
+            marginTop: '10px',
+            paddingLeft: '10px',
+            paddingBottom: '10px',
+            backgroundColor: 'lightgray',
+          }}
+        >
+          <h4
+            style={{ cursor: 'pointer' }}
+            onClick={() => navigate(`/post/${post.id}`)}
+          >
+            {post.title}
+          </h4>
+          <button onClick={() => navigate(`/post/${post.id}/edit`)}>
+            Edit
+          </button>
+          <button>Delete</button>
+        </div>
+      ))}
     </div>
   );
 };
